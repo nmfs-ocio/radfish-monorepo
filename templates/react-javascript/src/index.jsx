26216c67943f597c150cbb9193312236aae6e85d
Tony Gaskell <tony.gaskell@noaa.gov>
2026-04-30 13:00:40 -1000

    chore: npm-friendly variant of react-javascript template for @nmfs-radfish CLI

--- templates/react-javascript/src/index.jsx
+++ templates/react-javascript/src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Application } from "@nmfs-ocio/radfish";
+import { Application } from "@nmfs-radfish/radfish";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
--- templates/react-javascript/vite.config.js
+++ templates/react-javascript/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-import { radFishThemePlugin } from "@nmfs-ocio/vite-plugin-radfish-theme";
+import { radFishThemePlugin } from "@nmfs-radfish/vite-plugin-radfish-theme";
 
 /**
  * RADFish Theme Configuration
